cda8804a73da63c17afb055c80aaf3670748494b
Liban Osman <33349631+liban-osman@users.noreply.github.com>
2020-09-15 18:41:05 -0400

    Add files via upload

---
+++ Teams.py
@@ -0,0 +1,94 @@
+Teams = {
+    'Atlanta Hawks' : [
+        "Dewayne Dedmon",
+        "John Collins",
+        "De'Andre Hunter",
+        "Kevin Huerter",
+        "Trae Young"
+    ],
+    'Boston Celtics' : [
+        "Jayson Tatum",
+        "Marcus Smart",
+        "Kemba Walker",
+        "Daniel Theis",
+        "Gordon Hayward"
+    ],
+    'Brooklyn Nets' : [
+        "DeAndre Jordan",
+        "Wilson Chandler",
+        "Caris LeVert",
+        "Joe Harris",
+        "Spencer Dinwiddie"
+    ],
+    'Charlotte Hornets' : [
+        "Cody Martin",
+        "Cody Zeller",
+        "Malik Monk",
+        "Miles Bridges",
+        "Nicolas Batum"
+    ],
+    'Chicago Bulls' : [
+        "Wendell Carter Jr.",
+        "Lauri Markkanen",
+        "Shaquille Harrison",
+        "Denzel Valentine",
+        "Coby White"
+    ],
+    'Cleveland Cavaliers' : [
+        "Andre Drummond",
+        "Kevin Love",
+        "Cedi Osman",
+        "Collin Sexton",
+        "Matthew Dellavedova"
+    ],
+    'Dallas Mavericks' : [
+        "Boban Marjanovic",
+        "Luka Doncic",
+        "Justin Jackson",
+        "Kristaps Porzingis",
+        "Maxi Kleber"
+    ],
+    "Washington Wizards" : [
+        "Thomas Bryant", 
+        "Rui Hachimura", 
+        "Jerome Robinson", 
+        "Bradley Beal", 
+        "Shabazz Napier"
+        ],
+    "Utah Jazz" : [
+        "Rudy Gobert", 
+        "Bojan Bogdanovic", 
+        "Royce O'Neale", 
+        "Donovan Mitchell", 
+        "Mike Conley"
+        ],
+    "Toronto Raptors" :[
+        "OG Anunoby",
+        "Fred VanVleet",
+        "Kyle Lowry",
+        "Norman Powell",
+        "Serge Ibaka"
+        ],
+    "San Antonio Spurs" : [
+        "LaMarcus Aldridge", 
+        "Dejounte Murray",
+        "Trey Lyles", 
+        "DeMar DeRozan", 
+        "Bryn Forbes"
+        ],
+    "Sacramento Kings" : [
+        "Richaun Holmes", 
+        "Nemanja Bjelica", 
+        "Harrison Barnes", 
+        "Bogdan Bogdanovic", 
+        "De'Aaron Fox"
+        ],
+    "Portland Trail Blazers" : [
+        "Hassan Whiteside", 
+        "Carmelo Anthony", 
+        "Trevor Ariza", 
+        "CJ McCollum", 
+        "Damian Lillard"
+        ]
+}
+
